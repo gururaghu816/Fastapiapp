b664311cd717893b5d3ecd286541cb9f464b8551
Gurunath Reddy <gururaghu816@gmail.com>
2026-07-09 11:29:14 +0530

    git

--- backend/utils/oauth2.py
+++ backend/utils/oauth2.py
@@ -1,23 +1,36 @@
-from fastapi import Depends,HTTPException
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
 from database import get_db
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select 
 from utils.token import verify_access_token
 from models.users import User
 
-oauth2_scheme=OAuth2PasswordBearer(tokenUrl="auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
-async def get_current_user(token: str=Depends(oauth2_scheme),db:AsyncSession=Depends(get_db)):
-    user_info=verify_access_token(token)
-    result=await db.execute(select(User).filter (User.id ==int (user_info["sub"])))
-    current_user=result.scalers().first()
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    user_info = verify_access_token(token)
+
+    result = db.execute(
+        select(User).filter(User.id == int(user_info["sub"]))
+    )
+
+    current_user = result.scalars().first()
+
     if current_user is None:
-        raise HTTPException(status_code=401,detail="Invalid credentials")
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
     return current_user
-def role_required(roles:list):
+
+
+def role_required(roles: list):
     def role_decorator(current_user=Depends(get_current_user)):
         if current_user.role not in roles:
-            raise HTTPException(status_code=403,detail="Access denied")
+            raise HTTPException(status_code=403, detail="Access denied")
         return current_user
-    return role_decorator   
+
+    return role_decorator
--- backend/utils/token.py
+++ backend/utils/token.py
@@ -5,21 +5,19 @@ from dotenv import load_dotenv
 import os
 from models import users
 from fastapi import Depends,HTTPException
-from sqlalchemy.orm import Session
-from database import get_db
 load_dotenv()
-GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+SECRET_KEY=os.getenv("SECRET_KEY")
 ALGORITHM=os.getenv("ALGORITHM")
 
 def create_access_token(data:dict,expires_delta:timedelta=timedelta(hours=2)):
     to_encode=data.copy()
     expire=datetime.now()+expires_delta
     to_encode.update({"exp":expire})
-    encoded_jwt=jwt.encode(to_encode,key=GROQ_API_KEY,algorithm=ALGORITHM)
+    encoded_jwt=jwt.encode(to_encode,key=SECRET_KEY,algorithm=ALGORITHM)
     return encoded_jwt
 def verify_access_token(token:str):
     try:
-        to_decode=jwt.decode(token,GROQ_API_KEY,algorithms=[ALGORITHM])
+        to_decode=jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
         return to_decode
     except Exception as e:
         raise HTTPException(status_code=401,detail="Invalid credentials")
